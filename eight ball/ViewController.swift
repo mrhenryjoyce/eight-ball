dadd27899ce3eec45debef0e8405ba48cccbe42c
Kathy Joyce <kathy@hedwig-2.local>
2017-09-10 00:15:04 -0700

    This is the hello world test to try out Xcode and phone apps on actually phone

--- eight ball/ViewController.swift
+++ eight ball/ViewController.swift
@@ -1,4 +1,4 @@
-//
+////
 //  ViewController.swift
 //  eight ball
 //
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var ourLabel: UILabel!
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        print("Hello World")
+        ourLabel.text = "Hello World"
+        //UIImageView.image = UIImage(named: "hello.png")
+        //let imgView = NSImageView(frame: NSRect(x: 0, y:0, width: 300, height: 300))
+        //imgView.image = NSImage(named: "hello.png")
+        //self.view.addSubview(imgView)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //print("Hello World")
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(red: 178/255, green: 178/255, blue: 122/255, alpha: 1)
+        //self.view.backgroundColor = UIColor(patternImage: UIImage(named: "hello.png")!)
     }
 
     override func didReceiveMemoryWarning() {
